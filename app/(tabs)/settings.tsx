b623e586bb37fb4aaac2056a4f212aaf3fbf2c8d
Marcel Yuwono <54499847+mcay23@users.noreply.github.com>
2026-02-05 10:02:05 +0700

    eas updates

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -6,15 +6,32 @@ import {
   Pressable,
   TouchableHighlight,
   TouchableOpacity,
+  Alert,
 } from "react-native";
 import React, { useState, useEffect } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useSession } from "@/services/ctx";
 import { getSetting, setSetting } from "@/stores/settingsStore";
+import * as Updates from "expo-updates";
 
 export default function Settings() {
   const { signOut } = useSession();
   const [player, setPlayer] = useState<string>("exoplayer");
+
+  async function onFetchUpdateAsync() {
+    try {
+      const update = await Updates.checkForUpdateAsync();
+      if (update.isAvailable) {
+        await Updates.fetchUpdateAsync();
+        await Updates.reloadAsync();
+        Alert.alert("Update applied successfully!");
+      } else {
+        Alert.alert("No updates found");
+      }
+    } catch (error) {
+      alert(`Error fetching latest Expo update: ${error}`);
+    }
+  }
 
   useEffect(() => {
     getSetting("player").then((val) => {
@@ -39,20 +56,26 @@ export default function Settings() {
       <Text className="text-white">Platform: {Platform.OS}</Text>
       <Text className="text-white">Player: {player}</Text>
       <TouchableOpacity
-        onPress={() => handleSetPlayer("mpv")}
+        onPress={() => onFetchUpdateAsync()}
         hasTVPreferredFocus
-        className={`mt-3 p-2 rounded-lg border-2 ${player === "mpv" ? "bg-blue-700 border-white" : "bg-blue-500 border-transparent"} focus:border-white`}
+        className={`mt-3 p-2 rounded-lg bg-blue-500 border-2 border-transparent focus:border-white`}
+      >
+        <Text className="text-white">Fetch Updates</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => handleSetPlayer("mpv")}
+        className={`mt-3 p-2 rounded-lg bg-blue-500 border-2 border-transparent focus:border-white`}
       >
         <Text className="text-white">set mpv</Text>
       </TouchableOpacity>
       <TouchableOpacity
         onPress={() => handleSetPlayer("exoplayer")}
-        className={`mt-3 p-2 rounded-lg border-2 ${player === "exoplayer" ? "bg-blue-700 border-white" : "bg-blue-500 border-transparent"} focus:border-white`}
+        className={`mt-3 p-2 rounded-lg bg-blue-500 border-2 border-transparent focus:border-white`}
       >
         <Text className="text-white">set exoplayer</Text>
       </TouchableOpacity>
       <TouchableOpacity
-        className="mt-3 bg-blue-500 p-2 rounded-lg border-2 border-transparent focus:border-white"
+        className={`mt-3 p-2 rounded-lg bg-blue-500 border-2 border-transparent focus:border-white`}
         onPress={signOut}
       >
         <Text className="text-white">Sign Out</Text>
